d324bcfbed4e2a9aee732e681b7c1779428a0e80
Alessandre <aleliv74@gmail.com>
2025-06-17 07:36:44 -0300

    modulo 3 - Icons Customizados

---
+++ app/(protected)/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import { CityCard } from '@/src/components/CityCard'
+import { Icon } from '@/src/components/Icon'
+import { Screen } from '@/src/components/Screen'
+import { cityPreviewList } from '@/src/data/cities'
+import { CityPreview } from '@/src/types'
+import { FlatList, ListRenderItemInfo } from 'react-native'
+
+export default function HomeScreen() {
+  function renderItem({ item }: ListRenderItemInfo<CityPreview>) {
+    return <CityCard cityPreview={item} />
+  }
+
+  return (
+    <Screen>
+      <Icon name="Beach" />
+      <Icon name="Culture" />
+      {/* <Text variant="title28">Barcelona</Text> */}
+      <FlatList data={cityPreviewList} renderItem={renderItem} />
+    </Screen>
+  )
+}
---
+++ src/components/Icon.tsx
@@ -0,0 +1,48 @@
+// import iconConfig from '@/assets/icons/selection.json'
+import createIconSetFromIcoMoon from '@expo/vector-icons/createIconSetFromIcoMoon'
+import { ThemeColors } from '../theme/theme'
+import { useAppTheme } from '../theme/useAppTheme'
+
+const IconFromIcoMoon = createIconSetFromIcoMoon(require('@/assets/icons/selection.json'), 'IcoMoon', 'icomoon.ttf')
+
+// const IconNamme = iconConfig.icons.map((icon) => icon.properties.name)
+
+// console.log(IconNamme)
+export type IconProps = {
+  name: IconName
+  color?: ThemeColors
+  size?: number
+}
+
+export function Icon({ name, color = 'gray2', size = 24 }: IconProps) {
+  const { colors } = useAppTheme()
+
+  return <IconFromIcoMoon name={name} size={size} color={colors[color]} />
+}
+
+export type IconName =
+  | 'Adventure'
+  | 'Beach'
+  | 'Chevron-down'
+  | 'Chevron-left'
+  | 'Chevron-right'
+  | 'Chevron-up'
+  | 'Close'
+  | 'Culture'
+  | 'Explore'
+  | 'Favorite-fill'
+  | 'Favorite-outline'
+  | 'Gastronomy'
+  | 'Group'
+  | 'History'
+  | 'Home-fill'
+  | 'Home-outline'
+  | 'Luxury'
+  | 'Nature'
+  | 'Search-outline'
+  | 'Shopping'
+  | 'Star'
+  | 'Urban'
+  | 'Person-fill'
+  | 'Person-outline'
+  | 'Logout'
